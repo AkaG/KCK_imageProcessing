ce0858354484d56d9fc0c962d0280ee2d9f50dc1
Jakub Stańczak <kuba0095@gmail.com>
2016-11-23 14:47:46 +0100

    Fix overlaying image

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import numpy as np
 import sys
 import cv2
 
+def overlayImage(imgToOverlay, imgOverlying, x_offset, y_offset):
+    rgbaFrame = cv2.cvtColor(imgToOverlay, cv2.COLOR_BGR2RGBA)
+    for c in range(0,3):
+        rgbaFrame[y_offset:y_offset + imgOverlying.shape[0], x_offset:x_offset + imgOverlying.shape[1], c] =\
+            imgOverlying[:, :, c] * (imgOverlying[:, :, 3] / 255.0) +\
+            rgbaFrame[y_offset:y_offset + imgOverlying.shape[0], x_offset:x_offset+ imgOverlying.shape[1], c] * (1.0 - imgOverlying[:, :, 3] / 255.0)
+
+    return cv2.cvtColor(rgbaFrame, cv2.COLOR_RGBA2BGR)
+
 def main():
     facePath = "haarcascades/haarcascade_frontalface_default.xml"
     smilePath = "haarcascades/haarcascade_smile.xml"
@@ -30,16 +39,8 @@ def main():
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
             roi_gray = gray[y:y+h, x:x+w]
+            half_roi_gray = gray[y:y+h, x:x+int(w/2)]
             roi_color = frame[y:y+h, x:x+w]
-
-            eyes = eye_cascade.detectMultiScale(
-                roi_gray,
-                scaleFactor=1.5,
-                minNeighbors=6,
-                minSize=(55, 55)
-            )
-            for (ex,ey,ew,eh) in eyes:
-                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
             smile = smileCascade.detectMultiScale(
                 roi_gray,
@@ -47,16 +48,20 @@ def main():
                 minNeighbors=22,
                 minSize=(25, 25)
                 )
+            for (sx, sy, sw, sh) in smile:
+                cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (255, 0, 0), 1)
 
-            for (x, y, w, h) in smile:
-                cv2.rectangle(roi_color, (x, y), (x+w, y+h), (255, 0, 0), 1)
+            eyes = eye_cascade.detectMultiScale(
+                half_roi_gray,
+                scaleFactor=1.3,
+                minNeighbors=4,
+                minSize=(55, 55)
+                )
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+                frame = overlayImage(frame, googles, x+ex-20, y+ey-20)
 
-
-        rgbaFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-        for c in range(0,3):
-            rgbaFrame[0:0 + googles.shape[0], 0:0 + googles.shape[1], c] = googles[:, :, c] * (googles[:, :, 3] / 255.0) + rgbaFrame[0:0 + googles.shape[0], 0:0 + googles.shape[1], c] * (1.0 - googles[:, :, 3] / 255.0)
-
-        cv2.imshow('Smile Detector', cv2.cvtColor(rgbaFrame, cv2.COLOR_RGBA2BGR))
+        cv2.imshow('Smile Detector', frame)
         c = cv2.waitKey(7)
         if c == 27:
             break
